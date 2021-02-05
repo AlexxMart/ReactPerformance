ce9dd261216ea24293e27f01fd80319c41e43f0a
AlexxMart <alex@ravn.co>
2021-02-04 19:56:51 -0600

    Updated content

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -7,19 +7,21 @@ export const Home = () => (
 			<h1>Performance on React 🚀</h1>
 		</section>
 		<section className="slide">
-			<h1>Time-To-Interactive ⌛</h1>
+			<h1>RAIL Performance Model 🚃</h1>
+			<figure>
+				<img src="https://webdev.imgix.net/rail/rail.png" alt="RAILS model" />
+				<figcaption>
+					<a href="https://web.dev/rail/" target="_blank" rel="noreferrer noopener">RAIL Model</a>
+				</figcaption>
+			</figure>
 		</section>
 		<section className="slide">
-			<h1>RAIL Performance Mode 🚃</h1>
+			<h1>Time-To-Interactive ⌛</h1>
+			<a href="https://web.dev/interactive/" target="_blank" rel="noreferrer noopener">TTI</a>
 		</section>
 		<section className="slide">
 			<h1>Runtime Performance 🚅</h1>
-		</section>
-		<section className="slide">
-			<h1>The "Call Stack" 📥</h1>
-		</section>
-		<section className="slide">
-			<h1>Don't solve problems you're not facing 🔴</h1>
+			<a href="https://developers.google.com/web/tools/chrome-devtools/evaluate-performance" target="_blank" rel="noreferrer noopener">Evaluate performance</a>
 		</section>
 		<section className="slide">
 			<h1>How do we measure the performance of our app? 📏</h1>
